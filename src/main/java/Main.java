65725fb0be584ea14d3ca48f2b56f50de7e88594
fantom0x00F <hominem19@gmail.com>
2017-10-12 22:27:14 +0400

    aggregate requests/responses for convenient analysis

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,9 @@ import java.util.Queue;
 
 public class Main {
 
+    private static final int MAXIMUM_REQUEST_SIZE_IN_BYTES = 10 * 1024 * 1024;
+    private static final int MAXIMUM_RESPONSE_SIZE_IN_BYTES = 10 * 1024 * 1024;
+
     public static void main(String[] args) throws IOException, RootCertificateException {
         DefaultHttpProxyServer.bootstrap()
                 .withPort(8880)
@@ -28,6 +31,17 @@ public class Main {
                 .withAllowLocalOnly(false)
                 .withAllowRequestToOriginServer(true)
                 .withFiltersSource(new HttpFiltersSourceAdapter() {
+
+                    @Override
+                    public int getMaximumRequestBufferSizeInBytes() {
+                        return MAXIMUM_REQUEST_SIZE_IN_BYTES;
+                    }
+
+                    @Override
+                    public int getMaximumResponseBufferSizeInBytes() {
+                        return MAXIMUM_RESPONSE_SIZE_IN_BYTES;
+                    }
+
                     public HttpFilters filterRequest(HttpRequest originalRequest, ChannelHandlerContext ctx) {
                         return new HttpFiltersAdapter(originalRequest) {
 
